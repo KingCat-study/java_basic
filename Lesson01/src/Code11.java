b30cb1f5350523b4108e64acc8f410e48a58e77b
ljh <ljh@DESKTOP-BB0IQHC>
2020-08-27 20:18:55 +0900

    사용자로부터 먼저 정수의 개수 n을 입력받는다. 이어서 n개의 정 수를 입력받아 순서대로 배열에 저장한다. 그런 다음 중복된 정수 쌍의 개수를 카운트하여 출력하라. 예를 들어 n=6이고 입력된 정 수들이 2, 4, 2, 4, 5, 2이면 중복된 정수쌍은 (2,2), (2,2), (2,2), (4,4)로 모두 4쌍이다

---
+++ Lesson01/src/Code11.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+//사용자로부터 먼저 정수의 개수 n을 입력받는다. 이어서 n개의 정
+//수를 입력받아 순서대로 배열에 저장한다. 그런 다음 중복된 정수
+//쌍의 개수를 카운트하여 출력하라. 예를 들어 n=6이고 입력된 정
+//수들이 2, 4, 2, 4, 5, 2이면 중복된 정수쌍은 (2,2), (2,2),
+//(2,2), (4,4)로 모두 4쌍이다
+
+public class Code11 {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt();
+		int [] data = new int[n];
+		
+		for (int i = 0; i < data.length; i++) {
+			data[i] = kb.nextInt();
+		}
+		
+		kb.close();
+		
+		int count = 0;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = i+1; j < n; j++) {
+				if(data[i] == data[j]) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
