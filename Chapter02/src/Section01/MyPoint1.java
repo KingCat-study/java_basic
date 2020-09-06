8aa0c7c30144c1f9a626bfd7a4ed85bd76099828
KingCat-study <jihunrock@gmail.com>
2020-09-06 13:09:53 +0900

    code05 에서 사용

---
+++ Chapter02/src/Section01/MyPoint1.java
@@ -0,0 +1,11 @@
+package Section01;
+
+/*
+2차원 평면에서 정수 좌표를 가지는 하나의 점을 표현하기 위한
+클래스. 한 점의 x-좌표와 y-좌표는 항상 붙어다녀야 하므로 하
+나의 데이터 단위로 취급하기 위해서 이 클래스를 작성
+*/
+public class MyPoint1 {
+	public int x;
+	public int y;
+}
---
+++ Chapter02/src/Section01/MyRectangle1.java
@@ -0,0 +1,11 @@
+package Section01;
+
+/*
+좌표축에 평행한 하나의 사각형은 왼쪽-위쪽 꼭지점과 너비, 그
+리고 높이에 의해서 정의된다
+*/
+public class MyRectangle1 {
+	public MyPoint1 leftUpper;
+	public int width;
+	public int height;
+}
