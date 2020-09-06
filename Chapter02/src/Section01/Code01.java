1fc98abaf06c2a09105c6f72a47e9b974ab05f62
KingCat-study <jihunrock@gmail.com>
2020-09-06 10:47:09 +0900

    사용자 정의 타입 객체

---
+++ Chapter02/src/Section01/Code01.java
@@ -0,0 +1,14 @@
+package Section01;
+
+public class Code01 {
+
+	public static void main(String[] args) {
+		
+		Person1 first = new Person1();
+		
+		first.name = "John Lee";
+		first.number = "08109993333";
+		
+		System.out.println(first.name + " : "+first.number);
+	}
+}
